3b9fb9dd75db31f0bc564bc50885ebef50ad14a5
suyiiyii <suyiiyii@gmail.com>
2024-02-14 20:03:34 +0800

    updata CORS setting

--- code/server.py
+++ code/server.py
@@ -31,7 +31,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 允许所有来源
+    allow_origins=["wx.suyiiyii.top"],  # 允许所有来源
     allow_credentials=True,
     allow_methods=["*"],  # 允许所有方法
     allow_headers=["*"],  # 允许所有头部
