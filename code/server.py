2fb3a9a38abf2eb678d03a744adae3b78e68e476
suyiiyii <suyiiyii@gmail.com>
2024-02-14 21:19:10 +0800

    Add variable initialization and pre-deploy action in server.py and update name in s.yaml

--- code/server.py
+++ code/server.py
@@ -1,3 +1,4 @@
+a=1
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from fastapi.middleware.cors import CORSMiddleware
